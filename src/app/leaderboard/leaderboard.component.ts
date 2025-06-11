35bf347bb0b1e6de0053a9a3e28f6965e3883504
B33Gs <speschk@yahoo.com>
2025-06-11 14:28:11 -0400

    remove gapi and references

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { LeaderboardTableComponent } from '../leaderboard-table/leaderboard-table.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [LeaderboardTableComponent],
+  imports: [LeaderboardTableComponent, CommonModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css',
   host: { hostID: crypto.randomUUID().toString() },
 })
-export class LeaderboardComponent {
-  syncCalendar() {
-    console.log('clicked');
+export class LeaderboardComponent implements OnInit {
+  user: gapi.auth2.GoogleUser | null;
+
+  constructor(private ref: ChangeDetectorRef) {
+    this.user = null;
+  }
+
+  ngOnInit(): void {}
+
+  signIn() {
+    console.log('signIn called');
+  }
+
+  signOut() {
+    console.log('signOut called');
   }
 }
